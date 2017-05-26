9221b226f1908dd2ab22ba8b1fc9c71346a9a6da
Zhu Ming <mingzhu.z+github@gmail.com>
2017-05-26 18:10:05 +0800

    make demo of date range picker as a filter

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <span>Date range picker:</span>
-    <date-picker v-model="time2" range width="220" placeholder="from ~ to"></date-picker>
+    <div>
+      <span>Date range picker:</span>
+      <date-picker v-model="filterDateRange" range width="220" placeholder="from ~ to"></date-picker>
+    </div>
+    <div>
+      <span>filter: {{ filterText }}</span>
+    </div>
   </div>
 </template>
 
 <script>
 import DatePicker from 'vue2-datepicker'
+import moment from 'moment-timezone'
+import R from 'ramda'
 
 export default {
   name: 'app',
@@ -16,9 +23,31 @@ export default {
   data() {
     return {
       time1: '',
-      time2: '',
+      filterDateRange: '',
     }
   },
+  computed: {
+    filterText() {
+      console.log('filterDateRange = ', this.filterDateRange)
+      let filters = {};
+      if (!!this.filterDateRange && this.filterDateRange.length === 2) {
+        filters['from_date'] = moment(this.filterDateRange[0]).tz('Asia/Singapore').format('YYYY-MM-DD');
+        filters['to_date'] = moment(this.filterDateRange[1]).tz('Asia/Singapore').format('YYYY-MM-DD');
+      }
+
+      console.log('filters = ', filters)
+
+      let filterTexts = [];
+      const pushText = (value, column) => {
+        filterTexts.push(column + '|' + value);
+      }
+      R.forEachObjIndexed(pushText, filters);
+      const text = filterTexts.join(',');
+
+      console.log('filterTexts = ', filterTexts, ', text = ', text);
+      return text;
+    }
+  }
 }
 </script>
 
